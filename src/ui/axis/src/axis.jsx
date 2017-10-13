0a31e83cb998601f6cef94c2c2f8c42f0660874f
James Vermillion <vermijw@uw.edu>
2017-10-13 16:15:39 -0700

    updated doc on tickSizeInner to correlate with allowing functions as propType

--- src/ui/axis/src/axis.jsx
+++ src/ui/axis/src/axis.jsx
@@ -257,9 +257,12 @@ Axis.propTypes = {
   tickSize: PropTypes.number,
 
   /**
-   * [size of inner tick lines](https://github.com/d3/d3-axis#axis_tickSizeInner)
+   * Follows d3 convention if number [size of inner tick lines](https://github.com/d3/d3-axis#axis_tickSizeInner)
+   * If function that needs the value of a dynamic prop, it should be include a closure of the prop name as a string
+   * whose return function passed to this component expect its props to be passes with the following signature:
+   * signature: (props) => {}
    */
-  tickSizeInner: PropTypes.number,
+  tickSizeInner: PropTypes.oneOf([PropTypes.number, PropTypes.function]),
 
   /**
    * [size of outer tick lines](https://github.com/d3/d3-axis#axis_tickSizeOuter)
